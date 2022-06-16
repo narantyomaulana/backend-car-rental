c51225de714f86c7095b89e5a65529b5954be72a
narantyo maulana <olandeoxys@gmail.com>
2022-06-16 21:27:24 +0700

    update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,54 @@
-require('dotenv').config()
-module.exports = {
+require('dotenv').config();
+const {
+    DB_USER = '',
+    DB_PASSWORD = '',
+    DB_NAME = 'bcr',
+    DB_HOST = '127.0.0.1',
+    DB_PORT = '5432',
+  } = process.env;
+  
+  module.exports = {
     development: {
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: `${process.env.DB_NAME}_development`,
-        host: process.env.DB_HOST,
-        dialect: "postgres",
+      username: DB_USER,
+      password: DB_PASSWORD,
+      database: `${DB_NAME}`,
+      host: DB_HOST,
+      port: DB_PORT,
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
     },
-}
+    test: {
+      username: DB_USER,
+      password: DB_PASSWORD,
+      database: `${DB_NAME}`,
+      host: DB_HOST,
+      port: DB_PORT,
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    },
+    production: {
+      username: DB_USER,
+      password: DB_PASSWORD,
+      database: `${DB_NAME}`,
+      host: DB_HOST,
+      port: DB_PORT,
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    },
+  
+  };
